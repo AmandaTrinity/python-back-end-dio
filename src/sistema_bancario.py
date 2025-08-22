4d14ee8d6056b16a5e4cdfe75fe50a04d5749185
Amanda Trinity <amandatrinity16@gmail.com>
2025-08-22 10:42:11 -0300

    :bulb: docs: documenting functions and organizing

--- src/sistema_bancario.py
+++ src/sistema_bancario.py
@@ -1,7 +1,7 @@
 menu = """
 [MENU]
-[a] Nova Conta
-[c] Novo Usuário
+[a] Criar conta corrente
+[c] Cadastrar usuário
 [d] Depositar
 [s] Sacar
 [e] Extrato
@@ -19,10 +19,49 @@ extrato = ""
 numero_saques = 0
 LIMITE_NÚMEROS_SAQUES = 3
 
-# Objetivo geral: Separar funções existentes de saque,deposito e extrato em funções. Criar duas novas funções: cadastrar usuário e conta corrente
+# Operações bancárias
+def sacar(*, saldo, valor_saque, extrato, numero_saques):
+    """"Atualização de Estado. Key only"""
+    saldo -= valor_saque
+    numero_saques += 1
+    extrato += f'Valor sacado: R${valor_saque:.2f}\n'
+    print('Saque concluído.')    
+    return saldo, extrato, numero_saques
 
-#1. Validação de regras de negócio
+def depositar(saldo, valor_deposito, extrato, /):
+    if valor_deposito > 0:
+        saldo += valor_deposito
+        extrato += f'Valor depositado: R${valor_deposito:.2f}\n'
+        print('Depósito realizado')
+    else:
+        print('Por favor, digite um valor válido.')
+    return saldo, extrato
+
+def exibir_extrato(saldo, /, *, extrato):
+    print('\n=========== EXTRATO ===========')
+    print('Não foi realizado nenhum depósito ou saque.\n' if not extrato else extrato)
+    print(f'Saldo: R${saldo:.2f}')
+    print('================================')
+
+# Gerenciamento de Contas
+def criar_conta_corrente(agencia, numero_conta, conta_corrente, usuario, cpf):
+    conta_corrente[cpf] = {'agencia': agencia, 'numero_conta': numero_conta, 'usuario': usuario }
+    print("\nConta criada com sucesso!")
+    print(f"Agência: {agencia}\nC/C:     {numero_conta}")
+
+# Gerenciamento de Usuários
+def cadastro_usuario(*,cpf, nome, data_nascimento, endereco, usuarios_cadastrados):
+    """ Cadastro no  banco de dados"""
+    usuarios_cadastrados[cpf] = {'nome': nome, 'data_nascimento': data_nascimento, 'endereco': endereco}
+    print("\nUsuário criado com sucesso!")
+
+def filtrar_usuario(cpf, usuarios):
+    """"Buscar esse cpf no banco de dados para ver se já existe"""
+    return usuarios.get(cpf)
+
+# Validação de regras de negócio
 def possibilidade_sacar(*, valor_saque, saldo, numero_saques, limite_saque, limite_num_saques):
+    """Validação de regras de negócio"""
     excedeu_saldo = valor_saque > saldo
     excedeu_limite_valor = valor_saque > limite_saque
     excedeu_numero_saques = numero_saques >= limite_num_saques
@@ -42,43 +81,10 @@ def possibilidade_sacar(*, valor_saque, saldo, numero_saques, limite_saque, limi
     
     return False
 
-#2. Atualização de Estado. Key only
-def sacar(*, saldo, valor_saque, extrato, numero_saques):
-    saldo -= valor_saque
-    numero_saques += 1
-    extrato += f'Valor sacado: R${valor_saque:.2f}\n'
-    print('Saque concluído.')    
-    return saldo, extrato, numero_saques
-
-#positional only
-def depositar(saldo, valor_deposito, extrato, /):
-    if valor_deposito > 0:
-        saldo += valor_deposito
-        extrato += f'Valor depositado: R${valor_deposito:.2f}\n'
-        print('Depósito realizado')
-    else:
-        print('Por favor, digite um valor válido.')
-    return saldo, extrato
-
-#positional and keyword
-def exibir_extrato(saldo, /, *, extrato):
-    print('\n=========== EXTRATO ===========')
-    print('Não foi realizado nenhum depósito ou saque.\n' if not extrato else extrato)
-    print(f'Saldo: R${saldo:.2f}')
-    print('================================')
-
-# Cadastro no  banco de dados
-def cadastro_usuario(*,cpf, nome, data_nascimento, endereco, usuarios_cadastrados):
-    usuarios_cadastrados[cpf] = {'nome': nome, 'data_nascimento': data_nascimento, 'endereco': endereco}
-    print("\nUsuário criado com sucesso!")
-
-#Buscar esse cpf no banco de dados para ver se já existe
-def filtrar_usuario(cpf, usuarios):
-    return usuarios.get(cpf)
-
-# Lógica de negócio criação de usuário
+# Fluxo de Interação com o Usuário
 def fluxo_criacao_usuario(usuarios_cadastrados):
-    cpf = input('Digite o número do seu cpf (somente números): ')
+    """Lógica de negócio criação de usuário"""
+    cpf = int(input('Digite o número do seu cpf (somente números): '))
     
     if filtrar_usuario(cpf, usuarios_cadastrados):
         print('\nJá existe usuário com esse CPF!')
@@ -89,13 +95,8 @@ def fluxo_criacao_usuario(usuarios_cadastrados):
 
     cadastro_usuario(cpf=cpf, nome=nome, data_nascimento=data_nascimento, endereco=endereco, usuarios_cadastrados=usuarios_cadastrados)
 
-def criar_conta_corrente(agencia, numero_conta, conta_corrente, usuario, cpf):
-    conta_corrente[cpf] = {'agencia': agencia, 'numero_conta': numero_conta, 'usuario': usuario }
-    print("\nConta criada com sucesso!")
-    print(f"Agência: {agencia}\nC/C:     {numero_conta}")
-
 def fluxo_criacao_conta_corrente(conta_corrente, usuarios_cadastrados):
-    cpf = input('Digite o número do seu cpf (somente números): ')
+    cpf = int(input('Digite o número do seu cpf (somente números): '))
     usuario = filtrar_usuario(cpf, usuarios_cadastrados)
     if not usuario:
         print('Usuario não encontrado, fluxo de criação de conta encerrado')
@@ -105,6 +106,7 @@ def fluxo_criacao_conta_corrente(conta_corrente, usuarios_cadastrados):
 
     criar_conta_corrente(agencia, numero_conta, conta_corrente, usuario, cpf)
 
+# Lógica Principal
 while True:
 
     opcao = input(menu)
@@ -128,7 +130,6 @@ while True:
     
     elif opcao == "q":
         print('\nSaindo do sistema... Obrigado por usar o nosso serviço!')
-        break
     elif opcao == 'c':
         fluxo_criacao_usuario(usuarios_cadastrados)
     elif opcao == 'a':
